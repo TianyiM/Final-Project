5afae84035f1249df0cc1b172d91bc82a5359e5f
TianyiM <matianyi1029@gmail.com>
2016-05-04 10:41:53 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,18 @@
+import xlrd
+
+
+table_data = xlrd.open_workbook('~') 
+
+table = table_data.sheet_by_index(0) 
+
+col_data = table.col_values(3)
+
+raw_data = table.raw_values(2)
+
+row_num = table.nrows
+
+
+
+for i in col_data:
+ 
+   print i 
